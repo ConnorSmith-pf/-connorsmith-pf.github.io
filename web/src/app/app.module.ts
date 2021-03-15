ac3869bac6ee64672476c1a6e17ecc98f10bec81
Connor Smith <connor.github@gmail.com>
2021-03-15 21:54:17 +0000

    Cleaned up scss in header.component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RootComponent } from './components/root/root.component';
 import { RootModule } from './components/root/root.module';
+import { AppRouterModule } from './modules/router.module';
 import { SubscriptionsDirective } from './directives/subscriptions.directive';
 
 @NgModule({
 	declarations: [SubscriptionsDirective],
-	imports: [BrowserModule, BrowserAnimationsModule, RootModule],
+	imports: [AppRouterModule, BrowserModule, BrowserAnimationsModule, RootModule],
 	providers: [],
 	bootstrap: [RootComponent],
 })
